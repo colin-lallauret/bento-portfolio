9c1604b243f5e9f0121e52342dca94452a46f2a8
colin-lallauret <colinlallauret1@gmail.com>
2025-01-18 16:14:43 +0100

    Fix custom cursor + Add software icons

--- src/Components/Software.jsx
+++ src/Components/Software.jsx
@@ -1,14 +1,46 @@
 import React from "react";
 
+import Html from "../Assets/Images/Software/html.svg";
+import Css from "../Assets/Images/Software/css.svg";
+import JavaScript from "../Assets/Images/Software/javascript.svg";
+import Php from "../Assets/Images/Software/php.svg";
+import Twig from "../Assets/Images/Software/twig.svg";
+import Scss from "../Assets/Images/Software/scss.svg";
+import ReactLogo from "../Assets/Images/Software/react.svg";
+import Symfony from "../Assets/Images/Software/symfony.svg";
+
 function Software() {
   return (
     <div className="software">
       <div className="logo-slider">
         <div className="img-wrapper">
-          {[...Array(16)].map((_, index) => (
-            <div className="img-container" key={index}>
-              <img src="https://picsum.photos/201" alt={`Slider ${index}`} />
-            </div>
+          {[...Array(2)].map((_, i) => (
+            <React.Fragment key={i}>
+              <div className="img-container">
+                <img src={Html} alt="Html" />
+              </div>
+              <div className="img-container">
+                <img src={Css} alt="Css" />
+              </div>
+              <div className="img-container">
+                <img src={JavaScript} alt="JavaScript" />
+              </div>
+              <div className="img-container">
+                <img src={Php} alt="Php" />
+              </div>
+              <div className="img-container">
+                <img src={Twig} alt="Twig" />
+              </div>
+              <div className="img-container">
+                <img src={Scss} alt="Scss" />
+              </div>
+              <div className="img-container">
+                <img src={ReactLogo} alt="React" />
+              </div>
+              <div className="img-container">
+                <img src={Symfony} alt="Symfony" />
+              </div>
+            </React.Fragment>
           ))}
         </div>
       </div>
