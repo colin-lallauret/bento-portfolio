bc53cffbc3240e0aa00a1941be073e9eac159f0c
colin-lallauret <colinlallauret1@gmail.com>
2025-01-19 19:54:49 +0100

    Refaco & fix

--- src/Components/RollBanner.jsx
+++ src/Components/RollBanner.jsx
@@ -2,80 +2,19 @@ import React from "react";
 
 import Logo from "../Assets/Images/Logo/logo-black.svg";
 
-
 function RollBanner() {
   return (
     <div className="banner">
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
-      </div>
-
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
-      </div>
-
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
-      </div>
-
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
-      </div>
-
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
-      </div>
-
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
-      </div>
-
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
-      </div>
-
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
-      </div>
-
-      <div className="text-roll">
-        <img src={Logo} alt="logo" />
-        <div>
-          <p>PORTFOLIO</p>
-          <span>PERSONNEL</span>
-        </div>
+      <div className="scroll-content">
+        {[...Array(10)].map((_, index) => (
+          <div className="text-roll" key={index}>
+            <img src={Logo} alt="logo" />
+            <div>
+              <p>PORTFOLIO</p>
+              <span>PERSONNEL</span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
